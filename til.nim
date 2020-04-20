2bdb69644be7c3bd126dd9e6827ed9a4e06452e6
Daniel E. Cook <danielecook@gmail.com>
2020-04-20 12:19:46 -0400

    update til

--- til.nim
+++ til.nim
@@ -6,12 +6,25 @@ import argparse
 import osproc
 import tables
 import nre
+import times
 from posix import signal, SIG_PIPE, SIG_IGN
 signal(SIG_PIPE, SIG_IGN)
 
 const VERSION = "0.0.1"
 const TIL_DIR = getHomeDir().joinPath(".til")
 const README_PATH = getHomeDir().joinPath(".til").joinPath("README.md")
+
+type 
+    til_object = ref object
+        topic: string
+        title: string
+        fname: string
+        basename: string
+        description: string
+        date: times.Time
+
+proc `$`(til: til_object) =
+    echo fmt"""[{til.title}] {til.description} {til.date.format("yyyy-MM-dd")}"""
 
 # Init
 discard existsOrCreateDir(TIL_DIR)
@@ -43,36 +56,54 @@ proc editor_cmd(fname: string): string =
     if editor.startsWith("subl"):
         # sublime
         check_comm("subl")
-        comm = fmt"subl -w {fname}:3"
+        comm = fmt"subl -w {fname}:1:3"
     elif editor == "nano":
         # nano
         check_comm("nano")
-        comm = fmt"nano +3,0 {fname}"
+        comm = fmt"nano +0,2 {fname}"
     elif editor.startsWith("vi"):
         # vim / vi
         check_comm(editor)
-        comm = fmt"{editor} {fname} '+call cursor(3,0)'"
+        comm = fmt"{editor} {fname} '+call cursor(0,2)'"
     elif editor == "atom":
         # atom
         check_comm(editor)
-        comm = fmt"atom {fname}:3"
+        comm = fmt"atom {fname}:0:2"
     elif editor == "emacs":
         # emacs
         check_comm(editor)
-        comm = fmt"emacs +3:0 {fname}"
+        comm = fmt"emacs +0:2 {fname}"
     else:
         check_comm(editor)
         comm = fmt"{editor} {fname}"
     return comm
 
-proc list_tils(): Table[string, seq[string]] =
-    var all_tils = initTable[string, newSeq[string]()]()
+proc get_description(fname: string): string = 
+    # Extracts the description from the title
+    var f: File
+    var line: string
+    try:
+        discard open(f, fname, fmRead)
+        line = f.readLine()
+    finally:
+        close(f)
+    if line.startsWith("#"):
+        return line.strip(chars = {'#', ' '})
+    else:
+        return ""
+
+proc list_tils(): Table[string, seq[til_object]] =
+    var all_tils = initTable[string, newSeq[til_object]()]()
     for full_path in walkDirs(TIL_DIR.joinPath("/*")):
         let topic = full_path.lastPathPart()
         for til_fname in walkFiles(TIL_DIR.joinPath(topic & "/*")):
-            let til = til_fname.lastPathPart()[0..^4]
             if topic in all_tils == false:
                 all_tils[topic] = @[]
+            var til = til_object(fname: til_fname,
+                           topic: topic,
+                           title: til_fname.lastPathPart()[0..^4],
+                           description: get_description(til_fname),
+                           date: til_fname.getCreationTime())
             all_tils[topic].add(til)
     return all_tils
         
@@ -88,12 +119,12 @@ proc build_readme() =
     if open(f, README_PATH, fmWrite):
         try:
             f.writeLine("# TIL" & '\n')
-            f.writeLine(fmt"n TILs: {til_count}")
-            f.writeLine(fmt"n Topics: {til_set.len()}")
+            f.writeLine(fmt"TILs: {til_count}")
+            f.writeLine(fmt"Topics: {til_set.len()}")
             for topic in til_set.keys():
                 f.writeLine(fmt"## {topic}" & '\n')
                 for til in til_set[topic]:
-                    f.writeLine(fmt"* [{til}]({topic}/{til}.md) - DESCRIPTION")
+                    f.writeLine(fmt"""* [`{til.title}`]({til.topic}/{til.title}.md) {til.description} {til.date.format("yyyy-MM-dd")}""")
                 f.writeLine("")
         finally:
             close(f)
@@ -123,19 +154,36 @@ var p = newParser("til"):
                     var f: File
                     if open(f, fpath, fmWrite):
                         try:
-                            f.writeLine(fmt"# {title}" & '\n' & '\n')
+                            f.writeLine(fmt"# description" & '\n' & '\n')
                         finally:
                             close(f)
                 let result = execCmd(editor_cmd(fpath))
                 if result == 0:
                     success_msg(fmt"Successfully added {folder_name}/{title}")
 
+    command("ls"):
+        arg("query", nargs = -1, help="Term to match on")
+        run:
+            var til_set = list_tils()
+            var output: bool
+            echo fmt"""{("Group".fgGreen & "/title"):<40}{"description":<20}{"date":<20}""".fgGreen
+            echo "-".repeat(80)
+            for group, tils in til_set:
+                for til in tils:
+                    let gt = group.fgGreen & "/" & til.title
+                    if opts.query.len() > 0:
+                        output = (group == opts.query[0] or opts.query[0] in til.title)
+                    else:
+                        output = true    
+                    if output:
+                        echo fmt"""{gt:<40}{til.description:<40}{til.date.format("yyyy-MM-dd"):<20}"""
+
     command("push"):
         help("Push TILs to a repo")
         run:
             echo "Pushing"
 
-echo list_tils()
+#echo list_tils()
 build_readme()
 
 if commandLineParams().len() == 0:
